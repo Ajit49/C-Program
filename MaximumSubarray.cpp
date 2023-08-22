14269a380970e343557c6de9d8841665b03dd815
Ajit Kumar Gauraw <82594129+Ajit49@users.noreply.github.com>
2023-08-23 01:23:03 +0530

    Array

---
+++ MaximumSubarray.cpp
@@ -0,0 +1,27 @@
+// Print maximum subarray
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = j; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
