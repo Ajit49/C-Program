ed364c99d07752a1ff661ef273b69386bc0a92cb
Ajit Kumar Gauraw <82594129+Ajit49@users.noreply.github.com>
2023-09-19 11:26:33 +0530

    Bit Manipulation

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-// using namespace std;
+using namespace std;
 
-// int clearBit(int n, int pos)
-// {
-//     int mask = ~(1 << pos);
-//     return (n & mask);
-// }
-// int main()
-// {
-//     cout << clearBit(5, 2) << endl;
-//     return 0;
-// }
+int clearBit(int n, int pos)
+{
+    int mask = ~(1 << pos);
+    return (n & mask);
+}
+int main()
+{
+    cout << clearBit(5, 2) << endl;
+    return 0;
+}
